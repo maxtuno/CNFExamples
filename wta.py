0560586a2d11082e7b5d495489b87309d1320292
PEQNP <oscar.riveros@peqnp.science>
2020-06-07 08:52:16 -0400

    Update wta.py

--- wta.py
+++ wta.py
@@ -3,8 +3,6 @@ import sys
 import numpy as np
 import peqnp.cnf as cnf
 
-# Weapon-Target Assignment Problem
-
 # Ref
 # http://i4c.iust.ac.ir/UPL/Paper18/i4c18-1008.pdf
 # http://web.karabuk.edu.tr/emrullahsonuc/wta/A_Parallel_Simulated_Annealing_Algorithm_for_Weapon-Target_Assignment_Problem.pdf
@@ -12,6 +10,8 @@ import peqnp.cnf as cnf
 # Instances
 # http://web.karabuk.edu.tr/emrullahsonuc/wta/
 
+def objective(e):
+    return sum(v[i] * np.prod([(1 - pij[i][j]) ** e[i][j] for j in range(n)]) for i in range(n))
 
 def load_instance(file_name):
     with open(file_name, 'r') as file:
@@ -25,7 +25,7 @@ def load_instance(file_name):
 
 
 if __name__ == '__main__':
-    n, v, pij = load_instance('data/test.txt')
+    n, v, pij = load_instance(sys.argv[1])
     alpha = 10 ** 4
     bits = int(alpha * sum(v) * pij.sum()).bit_length()
     opt = alpha * sum(v)
@@ -40,7 +40,7 @@ if __name__ == '__main__':
         cnf.end({'X': X})
         if cnf.satisfy(solver='java -jar -Xmx4g blue.jar'):
             opt = sum(v[i] * (alpha - pij[i][j] * alpha) if X.binary[i][j] else 1 for i in range(n) for j in range(n))
-            print(opt / alpha)
+            print(opt / alpha, objective(np.vectorize(int)(X.binary)))
             for i in range(n):
                 for j in range(n):
                     sys.stdout.write(str(int(X.binary[i][j])))
